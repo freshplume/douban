3d2dff33435f2bed9cfb39288fd2a2d03fb7f243
freshplume <freshplume@hotmail.com>
2020-12-31 18:05:36 +0800

    更新1231

---
+++ Test/TestXwlt.py
@@ -0,0 +1,15 @@
+
+import xlwt
+
+# workbook = xlwt.Workbook(encoding="UTF-8")  #创建wookbook对象
+# worksheet = workbook.add_sheet('sheet1')    #创建工作表
+# worksheet.write(0,0,'hello')                #写入数据，行、列、参数内容
+# workbook.save('student.xls')               #保存数据表
+
+workbook = xlwt.Workbook(encoding="UTF-8")  #创建wookbook对象
+worksheet = workbook.add_sheet('sheet1')    #创建工作表
+for i in range(0,9):
+    for j in range(0,i+1):
+        worksheet.write(i,j,"%d * %d = %d"%(i+1,j+1,(i+1)*(j+1)))
+
+workbook.save('student.xls')
